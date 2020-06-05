e6c3eb1c4ddd6ad582fb6aae5cf80beeedf9871b
pouya1991 <pouya.ahmadvand@gmail.com>
2020-06-05 00:04:44 -0700

    fix train config 2

--- 06_Train_1.sh
+++ 06_Train_1.sh
@@ -7,7 +7,7 @@
 #SBATCH -p rtx5000
 #SBATCH --gres=gpu:1
 #SBATCH --time=4-90:00:00
-#SBATCH --chdir /projects/ovcare/classification/pouya/components/PDAC_Binary_Pipeline
+#SBATCH --chdir /projects/ovcare/classification/pouya/components/docker_benchmark_pytorch
 
 source /home/poahmadvand/py3env/bin/activate
 python app.py --config_file_location "/projects/ovcare/classification/pouya/components/PDAC_Binary_Pipeline/06_Train_1.json"
