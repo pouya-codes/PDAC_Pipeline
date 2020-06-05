cef540b59c44022b214bd18bd90ffc375566a1e0
pouya1991 <pouya.ahmadvand@gmail.com>
2020-06-05 00:07:11 -0700

    fix train config 3

--- 06_Train_1.sh
+++ 06_Train_1.sh
@@ -10,6 +10,7 @@
 #SBATCH --chdir /projects/ovcare/classification/pouya/components/docker_benchmark_pytorch
 
 source /home/poahmadvand/py3env/bin/activate
+pip install -r requirements.txt
 python app.py --config_file_location "/projects/ovcare/classification/pouya/components/PDAC_Binary_Pipeline/06_Train_1.json"
 #kronos run \
 #	-c $PWD/../ \
